8f2c39bbd97ce6e79548cfa02f9db2804c446852
Rainbow <kenesou@gmail.com>
2023-05-02 00:16:00 +1200

    add the responsive of login and registe page

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,46 +1,40 @@
-import React from 'react';
+import React from "react";
 import "./LogIn.css";
-import { useState } from 'react';
+import { useState } from "react";
 
 const LogIn = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  
-  
 
   const handleClick = (e) => {
     e.preventDefault();
-    
   };
   return (
-    <div className='login'>
-        <div className='login__wrapper'>
-        <h1 id='login__wrapper--title'>SIGN IN</h1>
-        <form id='login__wrapper--form'>
-        <input
-            className='login__wrapper--input'
+    <div className="login">
+      <div className="login__wrapper">
+        <h1 id="login__wrapper--title">SIGN IN</h1>
+        <form id="login__wrapper__form">
+          <input
+            className="login__wrapper__form--input"
             placeholder="username"
             onChange={(e) => setUsername(e.target.value)}
           />
           <input
-          className='login__wrapper--input'
+            className="login__wrapper__form--input"
             placeholder="password"
-            type='password'
+            type="password"
             onChange={(e) => setPassword(e.target.value)}
           />
-          <button 
-          onClick={handleClick}
-          className='login__wrapper--button'
-          >
+          <button onClick={handleClick} className="login__wrapper__form--button">
             LOGIN
           </button>
-          
-          <a className='login__wrapper--link'>FORGOT PASSWORD?</a>
-          <a className='login__wrapper--link'>CREATE A NEW ACCOUNT</a>
+
+          <a className="login__wrapper__form--link">FORGOT PASSWORD?</a>
+          <a className="login__wrapper__form--link">CREATE A NEW ACCOUNT</a>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default LogIn;
