4f02bbc3ed586180447f1d8d780b4c34e255b7e9
Rainbow <kenesou@gmail.com>
2023-05-02 00:05:19 +1200

    finish register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import "./Register.css";
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div className="register">
+      <div className="register__wrapper">
+        <h1 id="register__wrapper--title">CREATE AN ACCOUNT</h1>
+        <form id="register__wrapper__form">
+          <input
+            className="register__wrapper__form--input"
+            placeholder="first name"
+          />
+          <input
+            className="register__wrapper__form--input"
+            placeholder="last name"
+          />
+          <input
+            className="register__wrapper__form--input"
+            placeholder="username"
+          />
+          <input
+            className="register__wrapper__form--input"
+            placeholder="email"
+          />
+          <input
+            className="register__wrapper__form--input"
+            placeholder="password"
+          />
+          <input
+            className="register__wrapper__form--input"
+            placeholder="confirm password"
+          />
+          <span className="register__wrapper__form--span">
+            By creating an account, I consent to the processing of my personal
+            data in accordance with the <b>PRIVACY POLICY</b>
+          </span>
+          <button id="register__wrapper__form--button">CREATE</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Register;
