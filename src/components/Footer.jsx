f4b21538796663e8c7286bc9f2580a53614dd74c
Rainbow <kenesou@gmail.com>
2023-05-04 18:27:18 +1200

    add the social icons in the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,32 @@
 import React from "react";
 import "./Footer.css";
+import {
+  AiFillFacebook,
+  AiFillInstagram,
+  AiFillTwitterSquare,
+} from "react-icons/ai";
+import { FaPinterestSquare } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <div className="footer">
       <div id="footer__copyright">
         <p>Copyright © 2023 by Rainbow Ou. All Rights Reserved.</p>
+        <div className="socialIconContainer">
+        
+          <div className="socialIconContainer__socialIcon" >
+          <AiFillFacebook className="socialIconContainer__socialIcon--fa" />
+          </div>
+          <div className="socialIconContainer__socialIcon">
+            <AiFillInstagram className="socialIconContainer__socialIcon--ins" />
+          </div>
+          <div className="socialIconContainer__socialIcon" color="55ACEE">
+            <AiFillTwitterSquare className="socialIconContainer__socialIcon--tw" />
+          </div>
+          <div className="socialIconContainer__socialIcon" color="E60023">
+            <FaPinterestSquare className="socialIconContainer__socialIcon--pin" />
+          </div>
+        </div>
       </div>
     </div>
   );
